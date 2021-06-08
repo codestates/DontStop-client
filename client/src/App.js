c375d6319551a70e010fdb3056509bd29185ac2d
lee-sangryeol <tkdfuf7600@gmail.com>
2021-06-08 23:28:14 +0900

    build : deploy 0.2

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>자 이제 시작이야 내꿈을 내꿈을 위한 여행 피카츄</h1>
+      <h1>자 이제 시작이야 내꿈을 내꿈을 위한 여행 피카츄!</h1>
     </div>
   );
 }
