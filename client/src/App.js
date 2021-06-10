9dc6811ee3ab002c8fd9095ab62d18e0cd44c200
Baymaxxxxx <pauliney3k@gmail.com>
2021-06-10 13:30:44 +0900

    SignInSignUp/0.1/first

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Main from './pages/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>자 이제 시작이야 내꿈을 내꿈을 위한 여행 피카츄!</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLogin: false,
+      accessToken: ""
+    }
+    this.loginHandler = this.loginHandler.bind(this)
+    this.issueAccessToken = this.issueAccessToken.bind(this);
+  }
+
+  loginHandler(data){
+    this.setState({ isLogin: true, accessToken: data })
+  }
+
+  issueAccessToken(token) {
+    this.setState({accessToken: token})
+  }
+
+  render() {
+    const { isLogin } = this.state;
+    return (
+      <div className='App'>
+        {isLogin ? (<Main accessToken={this.state.accessToken} issueAccessToken={this.issueAccessToken}/>) : (<SignIn loginHandler={this.loginHandler}/>)}
+      </div>
+    )
+  }
 }
 
 export default App;
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function SignUp() {
-    return (
-        <div>
-            
-        </div>
-    )
+class SignUp extends Component {
+    constructor(props) {
+        super(props)
+        this.setState = {
+            email: "",
+            password: "",
+            name: ""
+        }
+    }
+    
+    render() {
+        return (
+            <div className='Main'>
+                <div className='Signupwarp'>
+                    <div className='OauthsignupBtnwrap'>
+                        <button className='OauthsignupBtn' ><img src='img/googleLogo.png' alt='logo' className='google'/>Google</button>
+                    </div>
+                    <div className='inputField'>
+                        {/* <div className='col-25'>이메일</div> */}
+                        <input name='emailSignup'
+                        type='email'
+                        className='col-75'
+                        placeholder='Email'/>
+                    </div>
+                    <div className='inputField'>
+                        {/* <div className='col-25'>비밀번호</div> */}
+                        <input name='password'
+                        type='password'
+                        className='col-75'
+                        minlength='8'
+                        maxLength='16'
+                        placeholder='Password'/>
+                    </div>
+                    <div className='inputField'>
+                        {/* <div className='col-25'>이름</div> */}
+                        <input name='name'
+                        type='text'
+                        className='col-75'
+                        placeholder='Username'/>
+                    </div>
+                    <div className='SignupBtnwrap'>
+                        <input
+                        type='submit'
+                        className='SignupBtn' 
+                        value='가입하기'/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default SignUp
