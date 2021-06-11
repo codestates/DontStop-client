fb769b9d644b02302e02e6cbafefd03be74d7f93
Baymaxxxxx <pauliney3k@gmail.com>
2021-06-11 13:31:30 +0900

    OauthSigin/0.1/first

--- client/src/pages/SignInPage.js
+++ client/src/pages/SignInPage.js
@@ -1,10 +1,53 @@
-import React from   'react'
-import SignIn from './SignIn'
+import React from "react";
+import SignIn from "./SignIn";
 
 const SignInPage = () => {
-    return (
-        <SignIn></SignIn>
-    )
-}
+    return <SignIn></SignIn>;
+};
 
-export default SignInPage
+export default SignInPage;
+
+// **********App.js**********
+// import React, { Component } from "react";
+// import "./App.css";
+// import SignIn from "./pages/SignIn";
+// import SignUp from "./pages/SignUp";
+// import Main from "./pages/Main";
+
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             isLogin: false,
+//             accessToken: "",
+//         };
+//         this.loginHandler = this.loginHandler.bind(this);
+//         this.issueAccessToken = this.issueAccessToken.bind(this);
+//     }
+
+//     loginHandler(data) {
+//         this.setState({ isLogin: true, accessToken: data });
+//     }
+
+//     issueAccessToken(token) {
+//         this.setState({ accessToken: token });
+//     }
+
+//     render() {
+//         const { isLogin } = this.state;
+//         return (
+//             <div className="App">
+//                 {isLogin ? (
+//                     <Main
+//                         accessToken={this.state.accessToken}
+//                         issueAccessToken={this.issueAccessToken}
+//                     />
+//                 ) : (
+//                     <SignUp loginHandler={this.loginHandler} />
+//                 )}
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
