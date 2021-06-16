f5e5251484476f14028fe0ac426718cc2b5c24f0
Baymaxxxxx <pauliney3k@gmail.com>
2021-06-16 13:12:54 +0900

    afterrouter

---
+++ client/src/pages/Grouppage1.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import StudyHeader from "../components/StudySearch/StudyHeader";
+import Footer from "../components/Main/Footer";
+import GroupPost from "../components/GroupPage/GroupPost";
+import "../components/GroupPage/GroupPost.css";
+import { useSelector } from "react-redux";
+
+const Grouppage1 = () => {
+    const [groupposts, setGroupposts] = useState([]);
+
+    const token = useSelector(
+        (state) => state.userInfoReducer.userInfo.accessToken
+    );
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await axios.get("http://localhost:4000/groups/list", {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+            setGroupposts(res.data);
+            console.log("res", res.data);
+        }
+        fetchData();
+    }, []);
+    console.log("groupposts잘들어옴", groupposts);
+
+    const mapping = () => {};
+
+    return (
+        <>
+            <StudyHeader></StudyHeader>
+            <div className="grouppost-main">
+                <div className="space">
+                    {groupposts.map((el) => {
+                        return (
+                            <GroupPost
+                                title={el.title}
+                                contents={el.contents}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+            <Footer></Footer>
+        </>
+    );
+};
+
+export default Grouppage1;
