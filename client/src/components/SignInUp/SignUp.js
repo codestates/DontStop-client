b7bc79efb8b33236b502b1d78366a6643a5641ef
Yoo Rim Lee <yoolee741@gmail.com>
2021-06-16 21:26:47 +0900

    MyPage/0.5/update

--- client/src/components/SignInUp/SignUp.js
+++ client/src/components/SignInUp/SignUp.js
@@ -1,38 +1,67 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./SignUp.css";
 import { Link } from "react-router-dom";
 import OauthSign from "../../pages/OauthSign";
+import axios from "axios";
 
-class SignUp extends Component {
-    constructor(props) {
-        super(props);
-        this.setState = {
-            email: "",
-            password: "",
-            name: "",
-        };
-    }
+const SignUp = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
-    render() {
-        return (
-            <div className="signupMain">
-                <div className="Signupwarp">
-                    <div class="tab-header">
-                        <div class="active">Sign Up</div>
-                        <div class="inactive">
-                            <Link
-                                to="/login"
-                                style={{
-                                    textDecoration: "none",
-                                    color: "black",
-                                }}
-                            >
-                                Sign In
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="OauthsignupBtnwrap">
-                        {/* <button className="OauthsignupBtn">
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    axios.post(
+      "http://localhost:4000/users/signup",
+      {
+        email: email,
+        password: password,
+        name: name,
+      },
+      {
+        headers: {
+          "content-type": "application/json",
+          withCredentials: true,
+        },
+      }
+    );
+    history.push("/login");
+  };
+
+  return (
+    <div className="signupMain">
+      <div className="Signupwarp">
+        <div class="tab-header">
+          <div class="active">Sign Up</div>
+          <div class="inactive">
+            <Link
+              to="/login"
+              style={{
+                textDecoration: "none",
+                color: "black",
+              }}
+            >
+              Sign In
+            </Link>
+          </div>
+        </div>
+        <div className="OauthsignupBtnwrap">
+          {/* <button className="OauthsignupBtn">
                             <img
                                 src="img/googleLogo.png"
                                 alt="logo"
@@ -40,68 +69,71 @@ class SignUp extends Component {
                             />
                             Google
                         </button> */}
-                        <OauthSign className="OauthsignBtn"></OauthSign>
-                    </div>
-                    <div className="oneline">
-                        <hr
-                            style={{
-                                backgroundColor: "#F2F2F2",
-                                width: 175,
-                                marginBottom: 40,
-                                marginRight: 10,
-                            }}
-                        />
-                        or
-                        <hr
-                            style={{
-                                backgroundColor: "#F2F2F2",
-                                width: 175,
-                                marginBottom: 40,
-                                marginLeft: 10,
-                            }}
-                        />
-                    </div>
-                    <div className="signupinputField">
-                        {/* <div className='col-25'>이메일</div> */}
-                        <input
-                            name="emailSignup"
-                            type="email"
-                            className="Signupinput"
-                            placeholder="Email"
-                        />
-                    </div>
-                    <div className="signupinputField">
-                        {/* <div className='col-25'>비밀번호</div> */}
-                        <input
-                            name="password"
-                            type="password"
-                            className="Signupinput"
-                            minlength="8"
-                            maxLength="16"
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div className="signupinputField">
-                        {/* <div className='col-25'>이름</div> */}
-                        <input
-                            name="name"
-                            type="text"
-                            className="Signupnameinput"
-                            // className="col-75"
-                            placeholder="Username"
-                        />
-                    </div>
-                    <div className="SignupBtnwrap">
-                        <input
-                            type="submit"
-                            className="SignupBtn"
-                            value="가입하기"
-                        />
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+          <OauthSign className="OauthsignBtn"></OauthSign>
+        </div>
+        <div className="oneline">
+          <hr
+            style={{
+              backgroundColor: "#F2F2F2",
+              width: 175,
+              marginBottom: 40,
+              marginRight: 10,
+            }}
+          />
+          or
+          <hr
+            style={{
+              backgroundColor: "#F2F2F2",
+              width: 175,
+              marginBottom: 40,
+              marginLeft: 10,
+            }}
+          />
+        </div>
+        <div className="signupinputField">
+          {/* <div className='col-25'>이메일</div> */}
+          <input
+            name="emailSignup"
+            type="email"
+            className="Signupinput"
+            placeholder="Email"
+            onChange={handleEmail}
+          />
+        </div>
+        <div className="signupinputField">
+          {/* <div className='col-25'>비밀번호</div> */}
+          <input
+            name="password"
+            type="password"
+            className="Signupinput"
+            minlength="8"
+            maxLength="16"
+            placeholder="Password"
+            onChange={handlePassword}
+          />
+        </div>
+        <div className="signupinputField">
+          {/* <div className='col-25'>이름</div> */}
+          <input
+            name="name"
+            type="text"
+            className="Signupnameinput"
+            // className="col-75"
+            placeholder="Username"
+            onChange={handleName}
+          />
+        </div>
+        <div className="SignupBtnwrap">
+          <input
+            type="submit"
+            className="SignupBtn"
+            value="가입하기"
+            onClick={handleClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SignUp;
--- client/src/pages/Mypage.js
+++ client/src/pages/Mypage.js
@@ -8,7 +8,9 @@ import { setUserInfo, toggleLoginStatus } from "../actions";
 
 const MyPage = (props) => {
   const dispatch = useDispatch();
-
+  const token = useSelector(
+    (state) => state.userInfoReducer.userInfo.accessToken
+  );
   console.log("프롭: ", props);
   /*
   ToDos
@@ -31,20 +33,20 @@ const MyPage = (props) => {
   const [startBtn, setStartBtn] = useState(false);
 
   // 이번주 공부량 서버에서 받아오기! (에러뜨네으아아ㅓ란어리ㅏ넝란어리ㅓㅏ니얼)
-  let acc_time = axios
-    .get("http://localhost:4000/users/time", {
-      headers: {
-        Authorization: `Bearer ${props.token}`,
-      },
-    })
-    .then((res) => {
-      console.log("레스: ", res);
-      return res;
-    })
-    .catch((err) => {
-      console.log("에러: ", err);
-    });
-  console.log("너는 누구십니까", acc_time);
+  // let acc_time = axios
+  //   .get("http://localhost:4000/users/time", {
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   })
+  //   .then((res) => {
+  //     console.log("레스: ", res);
+  //     return res;
+  //   })
+  //   .catch((err) => {
+  //     console.log("에러: ", err);
+  //   });
+  // console.log("너는 누구십니까", acc_time);
 
   const handleStartBtn = () => {
     // 2-1. 시작버튼 누르면 일시정지로 바뀜; 일시정지 -> 시작
@@ -72,13 +74,13 @@ const MyPage = (props) => {
     return () => clearInterval(stopWatch);
   });
 
-  const handleEndBtn = () => {
+  const handleEndBtn = async () => {
     // 종료 버튼을 누르면 시간을 서버로 post.. 어떻게?
     // 1초에 1000 으로 뜸
     setTime(0);
     setStartBtn(false);
-    console.log("타임: ", time);
-    axios.post(
+    // console.log("타임: ", time);
+    await axios.post(
       "http://localhost:4000/users/time",
       {
         time: time / 1000,
@@ -86,12 +88,26 @@ const MyPage = (props) => {
       {
         headers: {
           "content-type": "application/json",
-          withCredentials: true,
-          Authorization: `Bearer ${props.token}`,
+          //   withCredentials: true,
+          Authorization: `Bearer ${token}`,
         },
       }
     );
   };
+
+  let acc_time = axios
+    .get("http://localhost:4000/users/time", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => {
+      console.log("레스: ", res);
+      return res;
+    })
+    .catch((err) => {
+      console.log("에러: ", err);
+    });
 
   // ------------------------------------------------------------------------------------------------------
 
@@ -126,11 +142,12 @@ const MyPage = (props) => {
         {
           headers: {
             "content-type": "application/json",
-            withCredentials: true,
-            Authorization: `Bearer ${props.token}`,
+            // withCredentials: true,
+            Authorization: `Bearer ${token}`,
           },
         }
       );
+
       setTitle("");
       setContents("");
     }
@@ -139,21 +156,21 @@ const MyPage = (props) => {
   // -------------------------------------------------------------------
 
   // API 받아와보자
-  const [quote, setQuote] = useState("");
-  const getQuotes = () => {
-    const randomQuotes = `http://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
-    let allQuotes = axios
-      .get(randomQuotes)
-      .then((data) => {
-        // console.log("명언:", data);
-        let quoteData = data.data.quotes;
-        let randomNum = Math.floor(Math.random() * quoteData.length);
-        let randomQuote = data.data.quotes[randomNum];
+  // const [quote, setQuote] = useState("");
+  // const getQuotes = () => {
+  //   const randomQuotes = `https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
+  //   let allQuotes = axios
+  //     .get(randomQuotes)
+  //     .then((data) => {
+  //       // console.log("명언:", data);
+  //       let quoteData = data.data.quotes;
+  //       let randomNum = Math.floor(Math.random() * quoteData.length);
+  //       let randomQuote = data.data.quotes[randomNum];
 
-        setQuote(randomQuote.quote);
-      })
-      .catch((err) => console.log("명언에러: ", err));
-  };
+  //       setQuote(randomQuote.quote);
+  //     })
+  //     .catch((err) => console.log("명언에러: ", err));
+  // };
 
   // const [quote, setQuote] = useState("");
   // const getQuotes = () => {
@@ -208,21 +225,17 @@ const MyPage = (props) => {
         </Link>
         <Link to="./list">
           <span className="group1">
-            <button className="go1" onClick={getQuotes}>
-              그룹일지
-            </button>
+            <button className="go1">그룹일지</button>
           </span>
         </Link>
       </div>
       <div id="container1">
-        <button className="name1" onClick={getQuotes}>
-          {quote}
-        </button>
+        <button className="name1"></button>
         <div id="timer1">
           <div className="this_week">
             <span className="week1">이번주 나의 스터디</span>
 
-            <div className="acc_time1">{time / 1000}분</div>
+            <div className="acc_time1">분</div>
           </div>
 
           <div className="today1">
