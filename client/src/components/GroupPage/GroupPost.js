d67c5654c82b1dd09b30d44adb5e4ac821be61b7
wonbeenna <nwbnwb@naver.com>
2021-06-17 02:50:40 +0900

    ddddd

--- client/src/components/GroupPage/GroupPost.js
+++ client/src/components/GroupPage/GroupPost.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./GroupPost.css";
 
 function GroupPost({ title, contents, username, date }) {
+<<<<<<< HEAD
   // console.log("post", posts);
   console.log("title", title);
   console.log("contents", contents);
@@ -17,6 +18,28 @@ function GroupPost({ title, contents, username, date }) {
         <div className="user_name">
           <a>By </a>
           {username}
+=======
+    // console.log("post", posts);
+    console.log("title", title);
+    console.log("contents", contents);
+    console.log("user_name", username);
+    console.log("date", date);
+
+    return (
+        // <div className="grouppost-main">
+        //     <div className="space">
+        <div className="pppost">
+            <div className="group_userinfo">
+                <img className="userimg" src="img/DOGE.png" alt="" />
+                <div className="user_name">
+                    <a>By </a>
+                    {username}
+                </div>
+            </div>
+            <div className="pppost-title">{title}</div>
+            <div className="pppost-content">{contents}</div>
+            <div className="date">{date.slice(5, 10)}</div>
+>>>>>>> b98bf865d68878cc22fe00344d5060eb55544b9d
         </div>
       </div>
       <div className="pppost-title">{title}</div>
