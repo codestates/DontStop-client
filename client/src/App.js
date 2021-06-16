0e264be333e2584fa25358ff07daa0d2efb97629
wonbeenna <nwbnwb@naver.com>
2021-06-17 03:45:41 +0900

    merge-test3

--- client/src/App.js
+++ client/src/App.js
@@ -12,14 +12,13 @@ import SignUpPage from "./pages/SignUpPage";
 import Mypage from "./pages/Mypage";
 import StudySearch from "./pages/StudySearch";
 import UserInfo from "./pages/UserInfo";
-import Grouppage1 from "./pages/Grouppage1";
-import Login from "./components/SignInUp/Login";
+import Grouppage1 from "./pages/Grouppage";
 
 const App = () => {
   const isLogin = useSelector(
     (state) => state.loginStatusReducer.loginStatus.loginStatus
   );
-  //aa
+  //testtesttest
   return (
     <>
       <Router>
